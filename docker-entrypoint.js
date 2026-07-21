0bfc3c709905bb6ab8b9b281101cc662412d0712
Olumide Solanke <121975583+OluJakes@users.noreply.github.com>
2026-07-22 00:08:10 +0100

    Refactor docker-entrypoint.js for command execution

--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -1,26 +1,7 @@
 #!/usr/bin/env node
 
-  const { spawn } = require('node:child_process')
+const { spawn } = require('node:child_process')
 
-  const env = { ...process.env }
-
-;(async() => {
-    if (process.argv.slice(-2).join(' ') === 'node server.js') {
-          await exec('npx next build --experimental-build-mode generate')
-    }
-
-    await exec(process.argv.slice(2).join(' '))
-})()
-
-function exec(command) {
-    const child = spawn(command, { shell: true, stdio: 'inherit', env })
-        return new Promise((resolve, reject) => {
-              child.on('exit', code => {
-                      if (code === 0) {
-                                resolve()
-                      } else {
-                                reject(new Error(`${command} failed rc=${code}`))
-                      }
-              })
-        })
-}
+const command = process.argv.slice(2).join(' ')
+const child = spawn(command, { shell: true, stdio: 'inherit', env: process.env })
+child.on('exit', code => process.exit(code === null ? 1 : code))
